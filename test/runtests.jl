75a32ccef7f621f486e17e71011b180e011eebc6
YongHee Kim <masterplanf@gmail.com>
2020-06-14 16:11:05 +0900

    test on enforcing type

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,6 @@
 using Test, JSONPointer
 using OrderedCollections
 
-# construct
 @testset "construct Dict with JSONPointer" begin 
     p1 = j"/ba/1/a"
     p2 = j"/ba/2/a"
@@ -33,13 +32,46 @@ end
     @test data[j"/1/2"] == 20
     @test data[j"/1/4"] == ["me"]
     @test data[j"/1/4/1"] == "me"
+end
 
+
+@testset "Enforce type on JSONPointer" begin 
+    p1 = j"/a/1::String"
+    p2 = j"/a/2::Int"
+
+    data = Dict(p1 =>"this", p2 => 20)
+    @test data[p1] == "this"
+    @test data[p2] == 20
+   
+    @test_throws ErrorException data[p1] = 20
+    @test_throws ErrorException data[p2] = "this"
+
+    p1 = j"/a/1::Vector{Int}"
+    p2 = j"/a/2::Float64"
+    p3 = j"/a/3/k::Dict"
+    p4 = j"/b::Vector{String}"
+
+    data = Dict(p1 =>[10], p2 => 20, p3 => Dict(), p4 => ["this", "is"])
+    @test data[p1] == Int[10]
+    @test data[p2] == 20.
+    @test isa(data[p3], Dict)
+    @test data[p4] == ["this", "is"]
+
+    @test_throws ErrorException data[p3] = [100]
+    @test_throws ErrorException data[p4] = "this"
 end
 
 @testset "error handling" begin
     p1 = j"/1/a"
-    @test_throws ArgumentError Dict(p1)
+    @test_throws MethodError Dict(p1 => 10)
 
 end
 
 
+function foo(xs::(Pair{K,V} where K <: JSONPointer.Pointer where V)...) 
+    @show "OK?"
+end
+function foo(xs::Pair{K,V}...) where K where V 
+    @show "im not"
+    @show typeof.(xs)
+end
